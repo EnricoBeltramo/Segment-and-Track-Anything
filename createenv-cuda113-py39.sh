38810b95f5224a0923932f909b35864e6d8011dc
enrico.beltramo <enrico.beltramo@inventio.ai>
2024-01-27 12:33:53 +0100

    Minor fixes file installer

---
+++ createenv-cuda113-py39.sh
@@ -0,0 +1,15 @@
+eval "$(conda shell.bash hook)"
+conda remove -n sam-objectdetection-cu113-py39-env -y --all
+conda create -n sam-objectdetection-cu113-py39-env -y python=3.9
+conda activate sam-objectdetection-cu113-py39-env
+
+#conda install pytorch==1.10.1 torchvision==0.11.2 torchaudio==0.10.1 cudatoolkit=11.3 -c pytorch -c conda-forge -y
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+chmod +x ./script/install.sh
+./script/install.sh
+
+chmod +x ./script/download_ckpt.sh
+./script/download_ckpt.sh
+
+call conda deactivate
